f41e9e3340422a9c81e489082457bf269aa800c0
Victor Augusto <137521799+kakashiflw@users.noreply.github.com>
2023-08-03 15:02:34 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-function logar () {
+// const formulario = document.getElementById('formularios');
+// const lista = document.getElementById('listaDeCadastros');
 
-    const login = document.getElementById('login').value
-    const senha = document.getElementById9('senha').value;
+// function form () { 
 
-    if(login === "certo" && senha === "certo" ) {
-        alert("exito")
+//    formulario.addEventListener('submit', (evento) =>{
+//     evento.preventDefault();
+
+//     const nome = document.getElementById('seuNome').value;
+//     const data = document.getElementById('nascimento').value;
+//     const sexo = document.getElementById('genero').value;
+//     const endereço = document.getElementById('rua').value;
+//     const celular = document.getElementById('numero_tel').value;
+//     const login = document.getElementById('login').value;
+//     const senha =document.getElementById('senha').value;
+//     console.log (form);
+// })
+// }
+
+
+ const formulario = document.getElementById('formularios');
+ const nome = document.getElementById('seuNome');
+ const data = document.getElementById('nascimento');
+ const sexo = document.getElementById('genero');
+ const endereço = document.getElementById('rua');
+ const celular = document.getElementById('numero_tel');
+ const login = document.getElementById('login');
+ const senha = document.getElementById('senha');
+
+
+ formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+    if (nome.value === "" && data.value === "" && sexo.value === "" && endereço.value === "" &&  celular.value === "" && login.value === "" && senha.value === "") {
+        alert('seu nome é obrgatorio');
+        return;
     }
-}
 
-document.querySelector('#login').onclick = logar
+    formulario.submit();
+ });
